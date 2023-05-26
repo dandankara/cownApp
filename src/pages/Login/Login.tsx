f6e6e456632cc90ad3ede068a6fea1fff76b3dca
dandankara <cunhacb22@gmail.com>
2023-05-26 11:30:29 -0300

    feat: add page login

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+export default function Login() {
+
+  return (
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="dark" />
+        <Text>página de Login</Text>
+      </View>
+    </SafeAreaProvider>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
